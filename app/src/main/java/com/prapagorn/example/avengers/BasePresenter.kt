87dbb64a3211e4a1bd8aacb95e83061b358ce563
Snuxker <prapagorn@panya.me>
2019-03-06 13:11:17 +0700

    Removed unuse

--- app/src/main/java/com/prapagorn/example/avengers/BasePresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.prapagorn.example.avengers
-
-interface BasePresenter {
-
-    fun start()
-
-}
--- app/src/main/java/com/prapagorn/example/avengers/BaseScreen.kt
+++
@@ -1,7 +0,0 @@
-package com.prapagorn.example.avengers
-
-interface BaseScreen<T> {
-
-    var presenter: T
-
-}
